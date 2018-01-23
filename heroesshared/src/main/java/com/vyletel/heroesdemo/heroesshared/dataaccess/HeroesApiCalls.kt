c6beb465647160dba5c57d6634a806e4a9e79f28
Lukáš Vyletel <vyletel@gmail.com>
2018-01-23 19:59:04 +0100

    Add logging of HTTP calls

--- heroesshared/src/main/java/com/vyletel/heroesdemo/heroesshared/dataaccess/HeroesApiCalls.kt
+++ heroesshared/src/main/java/com/vyletel/heroesdemo/heroesshared/dataaccess/HeroesApiCalls.kt
@@ -2,13 +2,20 @@ package com.vyletel.heroesdemo.heroesshared.dataaccess
 
 import com.vyletel.network.getRetrofit
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 /**
  * Created by lukas on 13/01/2018.
  */
+
+val loggingInterceptor = HttpLoggingInterceptor().apply {
+    level = HttpLoggingInterceptor.Level.BASIC
+}
+
 const val MARVEL_API_URL = "http://gateway.marvel.com/v1/public/"
 val marvelHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
         .addInterceptor(MarvelHttpInterceptor())
         .build()
 
